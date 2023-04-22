f30a64c10c6d53dd7726ac2548dc8e58caa66c34
Maria Gromyko <mazvereva91@mail.ru>
2023-04-22 18:58:20 +0300

    first commit

---
+++ src/components/Loader.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Loader = () => (
+    <div class="spinner-border text-info" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { compose, applyMiddleware, legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { rootReducer } from './components/redux/rootReducer';
+import { forbiddenWordsMiddleware } from './components/redux/middleware';
+
+
+const store = createStore(rootReducer, compose (
+  applyMiddleware(thunk, forbiddenWordsMiddleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+
+const app = (
+  // <React.StrictMode>
+    <Provider store ={store}>
+      <App />
+    </Provider>
+  // </React.StrictMode>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
